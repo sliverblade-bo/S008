b1449e0e00b3859b7b3aaf72b461086e511ab1cb
SilverBlade2 <445788247@qq.com>
2017-10-15 17:16:21 +0800

    一波更新

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,21 @@
+#include "GameScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+Scene* GameScene::createScene()
+{
+    return GameScene::create();
+}
+
+bool GameScene::init()
+{
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+    
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    
+    return true;
+}
